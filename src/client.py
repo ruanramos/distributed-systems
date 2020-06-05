33428a6879902f85a29f9ea67adebd23e6c7529f
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-05 08:03:55 -0300

    First commit. Communication between client/server sockets working

---
+++ src/client.py
@@ -0,0 +1,16 @@
+import socket
+
+# For Client socket, always use this order:
+# socket()
+# connect()
+
+HOST = 'localhost'
+PORT = 50017
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+    clientSocket.connect((HOST, PORT))
+    print('Hello, are you the server? I am ' + HOST)
+    print("Can you wait for some seconds?")
+    clientSocket.send(b"5")
+    data = clientSocket.recv(1024)
+    print("Ok, I got your number. It was ", int(data))
