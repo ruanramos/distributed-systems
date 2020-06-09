41651c19f1dce94306495f53b21dff520e05c380
Ruan Ramos <ruan.ramos28@gmail.com>
2020-06-09 08:24:46 -0300

    Functional calculator for 2 numbers, exception when try to reuse clients connection

--- src/distributed_calculator/calculatorClient.py
+++ src/distributed_calculator/calculatorClient.py
@@ -5,21 +5,31 @@ import socket
 # connect()
 
 hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
+validOperators = ['-', '+', '*', '/']
 
 HOST = hostname
 PORT = 50017
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
-    clientSocket.settimeout(5)
     clientSocket.connect((HOST, PORT))
     print("Client connected to server!")
-    for i in range(3):
-        clientSocket.settimeout(5)
-        clientSocket.send(bytes(input("Enter a number"), 'utf8'))
+    while True:    
+        numbers = []
+        # kinda hardcoded for only 2 numbers and 1 operation
+        for i in range(2):
+            n = input("Give a number: ")
+            while not n.isnumeric:
+                n = input("Give a valid number: ")
+            numbers.append(n)
+        numbersMessage = ""
+        for number in numbers:
+            numbersMessage += number + " "
+        clientSocket.send(bytes(numbersMessage, 'utf8'))
 
-    clientSocket.settimeout(5)
-    data = clientSocket.recv(1024)
-    print("Client received answer: " + str(data, 'utf8'))
+        operator = input("Send an operator: ")
+        while operator not in validOperators:
+            operator = input("Send one of the four valid operators (+, -, /, *): ")
+        clientSocket.send(bytes(operator, 'utf8'))
+
+        result = clientSocket.recv(1024)
+        print("Result: " + str(result, 'utf8'))
--- src/distributed_calculator/calculatorServer.py
+++ src/distributed_calculator/calculatorServer.py
@@ -9,28 +9,34 @@ import time
 
 HOST = ""
 PORT = 50017
+validOperators = ['-', '+', '*', '/']
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
     HOST = socket.gethostbyname(socket.gethostname())
     print(HOST, socket.gethostname(), socket.gethostbyname(socket.gethostname()))
     serverSocket.bind((HOST, PORT))
     serverSocket.listen(5)
-    # waits for first connection can be blocking
-    serverSocket.settimeout(5)
-    conn, address = serverSocket.accept()
-    with conn:
-        receivedNumbers = []
-        print('Connected by', address)
-        for i in range(3):
-            conn.settimeout(5)
-            data = conn.recv(1024)  # waits for message, can be blocking
-            if not data:
-    	        break
-            receivedNumbers.append(int(str(data, 'utf8')))
-                
-        print("Server will now send the numbers received multiplied by 4")
-        ans = ""
-        for i in receivedNumbers:
-            ans += str(i * 4)  + " "
-        conn.send(bytes(ans, 'utf8'))
-
+    
+    while True:
+        # waits for first connection can be blocking
+        clientSocket, address = serverSocket.accept()
+        
+        with clientSocket:
+            print('Connected by', address)
+            
+            receivedNumbers = []
+            dataReceived = clientSocket.recv(1024)  # waits for data, can be blocking
+            try:
+                receivedNumbers = str(dataReceived, 'utf8').split()
+                print('Received Numbers ' + receivedNumbers[0] + " and " + receivedNumbers[1])
+            except:
+                clientSocket.close()
+            
+            operatorReceived = clientSocket.recv(1024) # waits for operation
+            operatorReceived = str(operatorReceived, 'utf8')
+            print('Received operator ' + operatorReceived)
+            
+            result = str(eval(receivedNumbers[0] + operatorReceived + receivedNumbers[1]))
+                    
+            print("Server sent the answer: " + result)
+            clientSocket.send(bytes(result, 'utf8'))
