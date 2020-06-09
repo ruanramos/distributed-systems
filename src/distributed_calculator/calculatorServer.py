cd57b8d7f2b14016148146174b1b89d03581b1f2
Ruan Ramos <ruan.ramos28@gmail.com>
2020-06-09 10:30:45 -0300

    Refactor server to receive expression from client

--- src/distributed_calculator/calculatorServer.py
+++ src/distributed_calculator/calculatorServer.py
@@ -8,10 +8,10 @@ import time
 # accept() -> can loop
 
 HOST = ""
-PORT = 50017
+PORT = 50002
 validOperators = ['-', '+', '*', '/']
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+def oldCode():
     HOST = socket.gethostbyname(socket.gethostname())
     print(HOST, socket.gethostname(), socket.gethostbyname(socket.gethostname()))
     serverSocket.bind((HOST, PORT))
@@ -30,7 +30,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
                 if not dataReceived:
                     break
                 receivedNumbers = str(dataReceived, 'utf8').split()
-                print(receivedNumbers)
                 print('Received Numbers ' + receivedNumbers[0] + " and " + receivedNumbers[1])
                 
                 operatorReceived = clientSocket.recv(1024) # waits for operation
@@ -44,3 +43,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
                 print("Server sent the answer: " + result)
                 clientSocket.send(bytes(result, 'utf8'))
     print("Connection to " + address + " closed")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+    #oldCode()
+    HOST = socket.gethostbyname(socket.gethostname())
+    print(HOST, socket.gethostname(), socket.gethostbyname(socket.gethostname()))
+    serverSocket.bind((HOST, PORT))
+    serverSocket.listen(5)
+
+    while True:
+        # waits for first connection can be blocking
+        print("Server is waiting for client connection")
+        serverSocket.settimeout(15)
+        clientSocket, address = serverSocket.accept()
+        
+        with clientSocket:
+            print('Connected by', address)
+            
+            while True:
+                dataReceived = clientSocket.recv(1024)  # waits for data, can be blocking
+                if not dataReceived:
+                    break
+                receivedExpression = str(dataReceived, 'utf8')                
+                try:
+                    result = str(eval(receivedExpression))
+                    print("Server sent the answer: " + result)
+                    clientSocket.send(bytes(result, 'utf8'))
+                except:
+                    if receivedExpression == 'q':
+                        print("Connection closed")
+                        clientSocket.send(b"c")
+                        break
+                    print("error on eval")
+                    clientSocket.send(b"error")
