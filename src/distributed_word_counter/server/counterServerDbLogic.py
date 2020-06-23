57180509b7c491e67ba41ac6ab3079e545919109
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-23 13:20:48 -0300

    Create db communication

--- src/distributed_word_counter/server/counterServerDbLogic.py
+++ src/distributed_word_counter/server/counterServerDbLogic.py
@@ -1,2 +1,17 @@
+from pymongo import MongoClient
+
+
 def getFile(filename):
-    # TODO
+    return texts.find_one({"name": filename})
+
+
+def getAllFiles():
+    return texts.find()
+
+
+if __name__ == "__main__":
+    client = MongoClient('localhost', 27017)
+    db = client.test_db
+    texts = db.texts
+
+    print(getFile("test1"))
