c42c640a33263fcdecdec0d5ab1e9f29046c2265
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-23 13:44:24 -0300

    Use OOP on server main method

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from distributed_word_counter.server.counterServerConnection import Connector
+from distributed_word_counter.server.counterServerLogic import OptionHandler
 
 HOST = ""
 PORT = 50011
 CON_TIMEOUT = 15
+LISTEN_BACKLOG = 1
 
 
 if __name__ == "__main__":
-    connector = Connector(HOST, PORT, CON_TIMEOUT)
-    clientSocket, address = connector.listenToClients()
+    connector = Connector(HOST, PORT, LISTEN_BACKLOG, CON_TIMEOUT)
+    clientSocket, address = connector.acceptConnections()
 
     with clientSocket:
         print('Connected by', address)
@@ -15,9 +17,7 @@ if __name__ == "__main__":
         receivedOption = int(str(clientSocket.recv(1024), 'utf8'))
         print(f"You chose option {receivedOption}")
 
-        #optionHandler = OptionHandler(receivedOption)
-
-        if receivedOption == 4:
-            clientSocket.send(bytes("close", 'utf8'))
+        optionHandler = OptionHandler(receivedOption, clientSocket)
+        optionHandler.manageOption()
 
     print(f"Closed Connection to {address}")
