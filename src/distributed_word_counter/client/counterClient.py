f9fde60cfe17f0a0f751f87f25aa0341015c4ac5
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-23 13:20:00 -0300

    Create client menu logic

--- src/distributed_word_counter/client/counterClient.py
+++ src/distributed_word_counter/client/counterClient.py
@@ -1,18 +1,46 @@
 import socket
 
-if __name__ == "__main__":
-    hostname = socket.gethostname()
-    HOST = hostname
-    PORT = 50002
 
+def showMenu():
+    print("""
+            1- See saved files
+            2- Choose a file to analize
+            3- Create a new file
+            4- Quit
+            """)
+
+
+def validOption(option):
+    return int(option) in range(1, 5)
+
+
+def getOption():
+    option = input()
+    while not validOption(option):
+        print("Invalid option. Please choose a valid option")
+        showMenu()
+        option = input()
+    return option
+
+
+if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+        HOST = socket.gethostbyname(socket.gethostname())
+        PORT = 50011
         clientSocket.connect((HOST, PORT))
         print("Client connected to server!")
-        while True:
-            clientSocket.send(bytes(getExpression(), 'utf8'))
-            result = clientSocket.recv(1024)
-            result = str(result, 'utf8')
-            if result == "c":
-                break
-            print("Result: " + result)
-        print("Client is closing connection")
+
+        # while True:
+        showMenu()
+        option = getOption()
+        clientSocket.send(bytes(option, 'utf8'))
+
+        answer = clientSocket.recv(1024)
+        if str(answer, 'utf8') == "close":
+            print("Quiting program!")
+            exit(0)
+        # result = str(result, 'utf8')
+        # if result == "c":
+        #     break
+        # print("Result: " + result)
+    print("Client is closing connection")
