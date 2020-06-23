23e8cf335c04a81f65efda64bcdde59bff9fc074
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-23 13:21:29 -0300

    Clean logic file

--- src/distributed_word_counter/server/counterServerLogic.py
+++ src/distributed_word_counter/server/counterServerLogic.py
@@ -1,44 +1,23 @@
-import socket
-import time
+from distributed_word_counter.server.counterServerDbLogic import getFile, getAllFiles
 
-# For Server socket, always use this order
-# socket()
-# bind()
-# listen()
-# accept() -> can loop
+# Here the logic of word counting will happen
 
-HOST = ""
-PORT = 50002
-CON_TIMEOUT = 15
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
-    HOST = socket.gethostbyname(socket.gethostname())
-    print(HOST, socket.gethostname(), socket.gethostbyname(socket.gethostname()))
-    serverSocket.bind((HOST, PORT))
-    serverSocket.listen(1)
+class OptionHandler():
+    def __init__(self, option):
+        super().__init__()
+        self.option = option
 
-    while True:
-        # waits for first connection can be blocking
-        print(f"Server is waiting for client connection. Timeout in {CON_TIMEOUT} seconds")
-        serverSocket.settimeout(CON_TIMEOUT)
-        clientSocket, address = serverSocket.accept()
-        
-        with clientSocket:
-            print('Connected by', address)
-            
-            while True:
-                dataReceived = clientSocket.recv(1024)  # waits for data, can be blocking
-                if not dataReceived:
-                    break
-                receivedExpression = str(dataReceived, 'utf8')                
-                try:
-                    result = str(eval(receivedExpression))
-                    print("Server sent the answer: " + result)
-                    clientSocket.send(bytes(result, 'utf8'))
-                except:
-                    if receivedExpression == 'q':
-                        print("Connection closed")
-                        clientSocket.send(b"c")
-                        break
-                    print("error on eval")
-                    clientSocket.send(b"error")
+    def manageOption(self):
+        if self.option == 1:
+            # list all saved files
+            pass
+        elif self.option == 2:
+            # choose file to analyze
+            pass
+        elif self.option == 3:
+            # create new file
+            pass
+        else:
+            # quit
+            pass
