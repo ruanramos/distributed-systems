1d6fca0b59dc794094da1fb55f15922286b84312
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-24 11:27:52 -0300

    Add separation and decoding of file text

--- src/distributed_word_counter/server/FileAnalizer.py
+++ src/distributed_word_counter/server/FileAnalizer.py
@@ -1,8 +1,13 @@
-class FileAnalizer():
-    def __init__(self, filePath):
-        super().__init__()
-        self.filePath = filePath
+from distributed_word_counter.server.DatabaseHandler import DatabaseHandler
 
-    @staticmethod
-    def analize():
-        pass
+
+class FileAnalizer():
+
+    def __init__(self):
+        super().__init__()
+
+    def analize(self, filename):
+        # Analize logic here
+        dbHandler = DatabaseHandler()
+        fileToAnalize = dbHandler.getFile(filename)[0]
+        fileText = fileToAnalize["value"].decode()
