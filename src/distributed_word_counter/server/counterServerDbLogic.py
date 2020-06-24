98926a47d829e6c840287eac5609ee90592d5067
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-24 04:11:39 -0300

    Add small comment

--- src/distributed_word_counter/server/counterServerDbLogic.py
+++ src/distributed_word_counter/server/counterServerDbLogic.py
@@ -5,6 +5,7 @@ class DatabaseQuerier():
     def __init__(self):
         super().__init__()
         self.client = MongoClient("mongodb://localhost:27017/")
+        # Database name and collection name being hardcoded for now
         self.db = self.client.test_db
         self.collection = self.db.texts
 
