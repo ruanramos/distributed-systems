a1114efcdac6a81268a599bb49b964a47ebd36b9
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-24 06:07:31 -0300

    Add a method to save all files on files dir to mongo db

--- src/distributed_word_counter/server/DatabaseHandler.py
+++ src/distributed_word_counter/server/DatabaseHandler.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import os
 
 
 class DatabaseHandler():
@@ -8,9 +9,33 @@ class DatabaseHandler():
         # Database name and collection name being hardcoded for now
         self.db = self.client.test_db
         self.collection = self.db.texts
+        self.filesPath = "/home/ruan/Codes/distributed-systems/src/distributed_word_counter/files"
 
     def getFile(self, filename):
         return self.collection.find_one({"name": filename})
 
     def getAllFiles(self):
         return self.collection.find()
+
+    def saveAllFiles(self):
+        # Save all files from the files folder to mongodb
+        # texts collection on test_db
+        for entry in os.scandir(self.filesPath):
+            if entry.path.endswith(".txt") and entry.is_file():
+                with open(entry.path, 'r') as f:
+                    fullName = f.name.split('/')[-1].split('.')
+                    self.collection.insert(
+                        {
+                            "name": fullName[0],
+                            "extension": fullName[1],
+                            "value": bytes(f.read(), 'utf8')
+                        }
+                    )
+                    print(f'Ok to file {f}')
+        return
+
+
+if __name__ == "__main__":
+    a = DatabaseHandler()
+    for f in a.getAllFiles():
+        print(f["value"][:300])
