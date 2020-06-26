2931cf5c268c2132ab303d6af104626d5afffc47
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-26 00:43:04 -0300

    Move server main

---
+++ src/distributed_word_counter/server/serverMain.py
@@ -0,0 +1,12 @@
+import sys
+from counterServerConnection import Connector
+
+
+if __name__ == "__main__":
+    HOST = ""
+    PORT = int(sys.argv[1])
+    CON_TIMEOUT = 15
+    LISTEN_BACKLOG = 5
+    connector = Connector(HOST, PORT, LISTEN_BACKLOG, CON_TIMEOUT)
+    connector.acceptConnections()
+    print(f"Closed Connection to {connector.address}")
