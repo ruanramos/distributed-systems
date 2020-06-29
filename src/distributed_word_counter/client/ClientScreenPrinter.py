faf33c37fbae67a01a3e5dd3f12600eb1602fdbf
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-29 19:11:41 -0300

    Create client printer class

---
+++ src/distributed_word_counter/client/ClientScreenPrinter.py
@@ -0,0 +1,22 @@
+class ClientScreenPrinter():
+
+    @classmethod
+    def showMenu(cls):
+        print("""
+        You can check the files that are saved and choose one to be analyzed.
+        The analysis will show you the most used words in the file.
+
+                1- See saved files
+                2- Choose a file to analize
+                3- Create a new file
+                4- Quit
+                """)
+
+    @classmethod
+    def calculateSpaces(cls, word, index, entry):
+        spaces = [0, 0]
+        spaces[0] = 4 - len(entry[0])
+        spaces[1] = 10 - len(word)
+        if index == 10 or index == 100 or index == 1000:
+            spaces[0] -= 1
+        return spaces
