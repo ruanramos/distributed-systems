f8086e19ce36da20fc22cd6a9bcd46b18c79fa4b
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-29 18:07:31 -0300

    Fix regex and normalize word comparison

--- src/distributed_word_counter/server/application/logic/FileAnalizer.py
+++ src/distributed_word_counter/server/application/logic/FileAnalizer.py
@@ -1,3 +1,6 @@
+import re
+
+
 class TextAnalizer():
 
     def __init__(self, text):
@@ -6,18 +9,21 @@ class TextAnalizer():
 
     def getWordsCount(self):
         count = {}
-        for word in self.text.split():
-            if word in count:
-                count[word] += 1
+        for word in re.split('[ ,.!?"\n-]*', self.text):
+            if word.upper() in count:
+                count[word.upper()] += 1
             else:
-                count[word] = 1
+                count[word.upper()] = 1
         return count
 
     def analize(self, numberOfWords):
         # Analize logic here
         obj = {}
         sortedCount = {k: v for k, v in sorted(
-            self.getWordsCount().items(), key=lambda item: item[1], reverse=True)}
+            self.getWordsCount().items(),
+            key=lambda item: item[1],
+            reverse=True
+        )}
         for i, j in enumerate(sortedCount.items()):
             if i >= numberOfWords:
                 break
