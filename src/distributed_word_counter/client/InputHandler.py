536a2cf66edb11cc490eb8ca3ee032a66d5b1974
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-30 02:36:35 -0300

    Improve input handler

--- src/distributed_word_counter/client/InputHandler.py
+++ src/distributed_word_counter/client/InputHandler.py
@@ -3,18 +3,37 @@ from ClientScreenPrinter import ClientScreenPrinter
 
 class InputHandler():
 
-    @classmethod
-    def validOption(cls, option):
+    def __init__(self, option):
+        super().__init__()
+        self.option = option
+
+    @staticmethod
+    def validOption(option):
         try:
             return int(option) in range(1, 5)
         except ValueError:
             return False
 
-    @classmethod
-    def getOption(cls):
+    @staticmethod
+    def getOption():
         option = input("Option: ")
         while not InputHandler.validOption(option):
             print("Invalid option. Please choose a valid option")
             ClientScreenPrinter.showMenu()
             option = input()
-        return option
+        return InputHandler(option)
+
+    def handleAnalysisOption(self):
+        clientRequestMessage = {
+            "option": self.option,
+            "filename": None,
+            "numToAnalize": 10,
+        }
+        if self.option == "2":
+            clientRequestMessage["filename"] = input(
+                "What is the name of the file or number in the saved files list?\nFile: ")
+            numToAnalize = input(
+                "How many words? (leave blank for 10)\nNumber of Words: ")
+            clientRequestMessage["numToAnalize"] = numToAnalize if numToAnalize.isnumeric(
+            ) else 10
+        return clientRequestMessage
