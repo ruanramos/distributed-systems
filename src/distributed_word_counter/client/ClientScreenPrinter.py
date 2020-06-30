8a29655cb62970eed95d3a29ed6841587616e683
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-06-30 02:38:21 -0300

    Improve client screen printer

--- src/distributed_word_counter/client/ClientScreenPrinter.py
+++ src/distributed_word_counter/client/ClientScreenPrinter.py
@@ -1,7 +1,7 @@
 class ClientScreenPrinter():
 
-    @classmethod
-    def showMenu(cls):
+    @staticmethod
+    def showMenu():
         print("""
         You can check the files that are saved and choose one to be analyzed.
         The analysis will show you the most used words in the file.
@@ -12,11 +12,42 @@ class ClientScreenPrinter():
                 4- Quit
                 """)
 
-    @classmethod
-    def calculateSpaces(cls, word, index, entry):
+    @staticmethod
+    def calculateSpaces(word, index, entry):
         spaces = [0, 0]
         spaces[0] = 4 - len(entry[0])
         spaces[1] = 10 - len(word)
         if index == 10 or index == 100 or index == 1000:
             spaces[0] -= 1
         return spaces
+
+    @staticmethod
+    def showSavedFilesList(files):
+        print("--------- These are the saved files --------\n")
+        for i in files:
+            print(i)
+        print("\n--------------------------------------------\n")
+
+    @staticmethod
+    def printAnalysisHeader(serverResponseMessage, numToAnalize):
+        print("\n--------------------------------------------\n")
+        print(
+            f"  Result of the analysis for file \"{serverResponseMessage['filename']}\": \n")
+        if serverResponseMessage['result'] != "File not found":
+            print(
+                f"  {numToAnalize} most used words:\n")
+
+    @staticmethod
+    def showAnalysisResult(result):
+        for entry in result.items():
+            word = entry[1][0].upper()
+            numOfTimes = entry[1][1]
+            index = int(entry[0]) + 1
+            spaces = ClientScreenPrinter.calculateSpaces(word, index, entry)
+            print(
+                f"  {index}{spaces[0] * ' '} - \"{word}\"{spaces[1] * ' '} -->   {numOfTimes} times")
+        print("\n\n--------------------------------------------")
+
+    @staticmethod
+    def logFileNotFoundError(message):
+        print(message)
